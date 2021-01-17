910fb21245a14605452acef5775a354d0a4a6db4
alonilk2 <63236463+alonilk2@users.noreply.github.com>
2021-01-17 23:40:01 +0200

    a

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -9,23 +9,19 @@ import Card from 'react-bootstrap/Card'
 
 
 function Category(props) {
-  const [Email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  return (  
-      <div>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180"/>
-      <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      </Card>
-      </div>
-  );
-}
+    return (  
+        <div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={props.imagePath}/>
+        <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>
+        Click here to see more {props.title}
+        </Card.Text>
+        <Button variant="primary">Find out more</Button>
+        </Card.Body>
+        </Card>
+        </div>
+    );
+  }
 export default Category;
--- src/Components/HomePageComponent.js
+++ src/Components/HomePageComponent.js
@@ -2,17 +2,32 @@ import React, {useEffect, useState} from 'react';
 import history from '../history';
 import {useDispatch, useSelector} from 'react-redux';
 import '../css/HomePage.css'
-import CategoryPhone from '../Components/CategoryPhone';
-import CategoryComputer from '../Components/CategoryComputer';
-import CategoryTablet from '../Components/CategoryTablet';
+import Category from '../Components/Category';
+import Axios from 'axios';
 
 function HomePageComponent(props) 
 {
+    const [categories, setCategories] = useState();
+    useEffect(() => {
+        return () => {
+            Axios.get("https://techstar12.herokuapp.com/fetch-categories")
+            .then(function(response) { 
+                if(response.data.success){
+                    setCategories(response.data.categories);
+                    console.log(categories);
+                }
+                else console.log("Error: could not fetch category list from server.");
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        }
+    })
     return (   
         <div className="HomePagecontainer">
-            <CategoryPhone />
-            <CategoryComputer />
-            <CategoryTablet />
+            <Category title="Phones" imagePath="" />
+            <Category title="Computer" imagePath="" />
+            <Category title="Tablet" imagePath="" />
         </div>
     );
 }
