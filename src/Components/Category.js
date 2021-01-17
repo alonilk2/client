1cb45f25e02f2bf1dc71442ebc44fae7ad93a49b
alonilk2 <63236463+alonilk2@users.noreply.github.com>
2021-01-18 00:48:27 +0200

    a

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -11,16 +11,16 @@ import Card from 'react-bootstrap/Card'
 function Category(props) {
     return (  
         <div>
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={props.imagePath}/>
-        <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-        Click here to see more {props.title}
-        </Card.Text>
-        <Button variant="primary">Find out more</Button>
-        </Card.Body>
-        </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={`./`+props.imagePath}/>
+            <Card.Body>
+              <Card.Title>{props.title}</Card.Title>
+              <Card.Text>
+              Click here to see more {props.title}
+              </Card.Text>
+              <Button variant="primary">Find out more</Button>
+            </Card.Body>
+          </Card>
         </div>
     );
   }
--- src/Components/HomePageComponent.js
+++ src/Components/HomePageComponent.js
@@ -1,34 +1,44 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component, useState} from 'react';
 import history from '../history';
 import {useDispatch, useSelector} from 'react-redux';
 import '../css/HomePage.css'
 import Category from '../Components/Category';
 import Axios from 'axios';
 
-function HomePageComponent(props) 
-{
-    const [categories, setCategories] = useState();
-    useEffect(() => {
-        return () => {
-            Axios.get("https://techstar12.herokuapp.com/fetch-categories")
-            .then(function(response) { 
-                if(response.data.success){
-                    setCategories(response.data.categories);
-                    console.log(categories);
-                }
-                else console.log("Error: could not fetch category list from server.");
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
+class HomePageComponent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {categories:[]};
+    }
+    async componentWillMount() {
+        this.categoryList = this.categoryList.bind(this);
+        try {
+            var catsToList = await Axios.get("https://techstar12.herokuapp.com/fetch_categories"); 
+            console.log(catsToList);
+            if(catsToList.data.success){
+                this.setState({categories: catsToList.data.result});
+                console.log(this.state.categories);
+            } else {
+                console.log("Error: could not fetch category list from server.");
+            }
+        } catch (error) {
+            console.log(error);
         }
-    })
-    return (   
-        <div className="HomePagecontainer">
-            <Category title="Phones" imagePath="" />
-            <Category title="Computer" imagePath="" />
-            <Category title="Tablet" imagePath="" />
-        </div>
-    );
+    }
+    categoryList = () => {
+        if(this.state.categories) {    
+            const list = this.state.categories.map((category) =>
+            <Category key={category.Name} title={category.Name} imagePath={category.imagePath}/>)
+            return (list);
+        }
+    }
+    render(){
+        return (
+            <div className="HomePagecontainer">
+                {this.categoryList()}
+            </div>
+        );
+    }
+
 }
 export default HomePageComponent;
